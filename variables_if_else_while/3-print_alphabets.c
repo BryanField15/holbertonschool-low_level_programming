4ea5ac54038eb30bc3795c8199ff9401ae7a11d4
BryanField15 <6367@holbertonstudents.com>
2023-02-20 17:16:36 -0800

    updated 3 to while loop

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,18 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'a' ; ch <= 'z' ; ch++)
-		putchar(ch);
-	for (ch = 'A' ; ch <= 'Z' ; ch++)
-		putchar(ch);
+	ch = 'a';
+		while (ch <= 'z')
+		{
+			printf("%c", ch);
+			ch = ch + 1;
+		}
+	ch = 'A';
+		while (ch <= 'Z')
+		{
+			printf("%c", ch);
+			ch = ch + 1;
+		}
 	putchar('\n');
 	return (0);
 }
