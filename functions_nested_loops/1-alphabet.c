055d0d0e86b4ca41ef49b04832d20071c994ba86
BryanField15 <6367@holbertonstudents.com>
2023-02-22 01:44:43 -0800

    task 1 solutions

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_alphabet - prints the alphabet
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch = ch + 1;
+	}
+	_putchar('\n');
+}
