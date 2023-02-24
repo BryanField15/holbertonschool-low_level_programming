4751d981f0cf23624368a101b2b816a9820b9737
BryanField15 <6367@holbertonstudents.com>
2023-02-24 15:38:35 -0800

    task 5 solutions

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *more_numbers - prints 0-14 10 times over
+ */
+
+void more_numbers(void)
+{
+	int i;
+	char ch;
+
+	i = 0;
+	while (i <= 9)
+	{
+		ch = 0;
+		while (ch <= 14)
+		{
+			if (ch > 9)
+			{
+				_putchar(ch / 10 + '0');
+				_putchar(ch % 10 + '0');
+			}
+			else
+				_putchar(ch + '0');
+			ch = ch + 1;
+		}
+		_putchar('\n');
+		i = i + 1;
+	}
+}
