c323ef91683cc02f1e4cb211f7e3174359ee7648
BryanField15 <6367@holbertonstudents.com>
2023-02-24 21:28:52 -0800

    task 7 solutions

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *print_diagonal -takes int n and prints a diagonal line of length n
+ */
+
+void print_diagonal(int n)
+{
+	char r;/*row*/
+	char c;/*column*/
+
+	r = 0;
+
+	while (n > 0)
+	{
+		c = r;
+		while (c > 0)
+		{
+			_putchar(' ');
+			c = c - 1;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		r = r + 1;
+		n = n - 1;
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
