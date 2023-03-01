24b649b7b3489613bef1306915e113b20c5dac6f
BryanField15 <6367@holbertonstudents.com>
2023-02-28 17:37:07 -0800

    task 6 solutions

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+*_strlen -returns lenght of a string
+*@s: -char input string
+*Return: (n) lenght of string
+*/
+
+int _strlen(char *s)
+{
+	int n;
+
+	n = 0;
+
+	while (*s != '\0')
+	{
+		s = s + 1;
+		n = n + 1;
+	}
+	return (n);
+}
+
+/**
+ *puts2 -prints alternate characters of a string
+ *@str: string to be analysed
+ */
+
+void puts2(char *str)
+{
+	int len;
+	int n;
+
+	len = _strlen(str) - 1;
+	n = 0;
+
+	while (n < len)
+	{
+		_putchar(str[n]);
+		n = n + 2;
+	}
+}
