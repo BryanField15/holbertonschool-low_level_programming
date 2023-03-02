0cb5ce8f1bc5469b70f9207054c0480b02c85772
BryanField15 <6367@holbertonstudents.com>
2023-03-02 03:10:11 -0800

    task 100 attempt

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *atoi -converts a string to an integer
+@s: string given to be converted
+*/
+
+int _atoi(char* s)
+{
+	int i = 0;
+	int n = 0;
+	int sign = 1;
+
+	while (s[i] == ' ')
+	{
+		i = i + 1;
+	}
+
+	if (s[i] == '-')
+	{
+		sign = -1;
+		i = i + 1;
+	}
+
+	else if (s[i] == '+')
+	{
+		i = i + 1;
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		n = n * 10 + (s[i] - '0');
+		i = i + 1;
+	}
+	return (sign * n);
+}
