269c60e563e25a631bb86b0f36fb7108979a3325
BryanField15 <6367@holbertonstudents.com>
2023-03-02 19:34:38 -0800

    task 1 solution

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,26 @@
 #include "main.h"
 
 /**
+ *_strlen -returns lenght of a string
+ *@s: char type lenght of string
+ *Return: (n) lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	n = 0;
+
+	while (*s != '\0')
+	{
+		s = s + 1;
+		n = n + 1;
+	}
+	return (n);
+}
+
+/**
  *_strncat - concatenates two strings
  *@dest: desitination array
  *@src: source array
@@ -10,18 +30,19 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
+	int dest_len = _strlen(dest);
 	int i;
 
 	i = 0;
 
 	while (i < n && src[i] != '\0')
 	{
-		dest[i] = src[i];
+		dest[dest_len + i] = src[i];
 		i = i + 1;
 	}
 	while (i < n)
 	{
-		dest[i] = '\0';
+		dest[dest_len + i] = '\0';
 		i = i + 1;
 	}
 	return (dest);
