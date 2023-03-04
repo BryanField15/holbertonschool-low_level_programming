add9fb08335ee54a3286371ae038e251be1fe8c5
BryanField15 <6367@holbertonstudents.com>
2023-03-03 22:28:24 -0800

    set i == 0

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,7 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
-		if (str[i] == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' ||
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' ||
 		    str[i] == '\n')
 		{
 			if (str[i] >= 'a' && str[i] <= 'z')
