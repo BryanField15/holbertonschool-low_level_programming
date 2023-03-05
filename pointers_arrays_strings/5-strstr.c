9a6ee6772b52d2cccf176e30baf17e1d83dd1d60
BryanField15 <6367@holbertonstudents.com>
2023-03-05 01:03:48 -0800

    task 5 solutions

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ *_strstr -finds the first occurrence of substring needle in the haystack
+ *@haystack: string to be searched
+ *@needle: substring to be searched for
+ *Return: pointer to the beginning of the located substring, or
+ *NULL if the substring is not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	if (haystack == NULL)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+	while (*haystack != '\0')
+	{
+		j = 0;
+		while (needle[j] != '\0')
+		{
+			j = 0;
+			while (haystack[i] == needle[j])
+			{
+				i = i + 1;
+				j = j + 1;
+				if (needle[j + 1] == '\0')
+				{
+					return (needle);
+				}
+			}
+			i = i + 1;
+		}
+	}
+	return (NULL);
+}
