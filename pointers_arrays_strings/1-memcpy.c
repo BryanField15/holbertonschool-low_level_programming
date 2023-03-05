62ea94d7c636c85cc4d58d7cc55e80e0ae047af9
BryanField15 <6367@holbertonstudents.com>
2023-03-04 20:20:02 -0800

    task 1 solutions

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_memcpy -copies n bytes from memory area src to memory area dest
+ *@dest: destination memory
+ *@src: source memory
+ *@n: size of memory copied from src to dest
+ *Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *csrc = (char *)src;
+	char *cdest = (char *)dest;
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		cdest[i] = csrc[i];
+		i = i + 1;
+		n = n - 1;
+	}
+	return (dest);
+}
