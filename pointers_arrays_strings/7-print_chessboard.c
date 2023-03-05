b5e8ea490fa65a747d035eaed1db4d5153351afa
BryanField15 <6367@holbertonstudents.com>
2023-03-05 02:02:26 -0800

    task 6 solutions

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_chessboard -prints a chessboard
+ *@a: 2d array chessboard size
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int y;
+	int x;
+
+	y = 0;
+	while (y < 8)
+	{
+		x = 0;
+		while (x < 8)
+		{
+			_putchar(a[y][x]);
+			x = x + 1;
+		}
+		_putchar('\n');
+		y = y + 1;
+	}
+}
