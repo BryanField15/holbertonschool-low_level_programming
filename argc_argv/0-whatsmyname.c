a95aafa53fbcfedf050bcd03634252b5e869faa2
BryanField15 <6367@holbertonstudents.com>
2023-03-06 19:51:40 -0800

    task 0 solution

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - function that prints its own name
+ *@argc: number of argumants in the command line
+ *@argv: array of strings that contain each of the commands in the command line
+ *Return: name of program
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc >=1)
+		printf("%s\n", argv[0]);
+	return (0);
+}
