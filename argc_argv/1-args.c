8f6921e41ca7ba264214551ed5c47c6f62ecd84c
BryanField15 <6367@holbertonstudents.com>
2023-03-08 15:02:05 -0800

    task 1 solution

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - function that prints its own name
+ *@argc: number of argumants in the command line
+ *@argv: array of strings that contain each of the commands in the command line
+ *Return: name of program
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+		printf("%d\n", argc);
+		return (0);
+}
