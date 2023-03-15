642d93083399b3cb6104e78eb1d7c3db88e1dfd4
BryanField15 <6367@holbertonstudents.com>
2023-03-15 15:13:36 -0700

    h or w = 0

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -13,22 +13,29 @@ int **alloc_grid(int width, int height)
 	int **mem;
 	int h;
 	int w;
-/*allocate memory for array of pointers to pointers*/
 
+	w = 0;
+	h = 0;
+
+	if (h < 0 || w < 0)
+		return (NULL);
+
+/*allocate memory for array of pointers to pointers*/
 	mem = malloc(sizeof(*mem) * height);
 	if (mem == NULL)
+
 		return (NULL);
+
 /*aloocate memory to the array of pointers*/
-	h = 0;
+
 	while (h < height)
 	{
 		mem[h] = malloc(sizeof(**mem) * width);
 		if (mem[h] == NULL)
-		{
 			return (NULL);
-		}
+
 /*initialize the array with given value*/
-		w = 0;
+
 		while (w < width)
 		{
 			mem[h][w] = 0;
