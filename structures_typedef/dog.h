d706e36650efdd76fbde3b5650dff2a0ecee78d6
BryanField15 <6367@holbertonstudents.com>
2023-03-19 16:23:18 -0700

    task 0 solution

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ *struct dog - dog details
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of dog
+ *Description: relevant details of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif /* _DOG_H_ */
