0bb3deab9313ca196bad2c7a6b9137d490cc1134
BryanField15 <6367@holbertonstudents.com>
2023-03-20 03:00:19 -0700

    header guard and null case

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,4 +1,7 @@
+#ifndef _STDDEF_
 #include <stddef.h>
+#endif /* _STDDEF_ */
+
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
 
@@ -6,4 +9,5 @@ void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int _putchar(char c);
+
 #endif /* _FUNCTION_POINTERS_H_ */
