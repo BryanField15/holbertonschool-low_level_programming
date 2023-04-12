47fad945970d3aa4aab284b3bf822971fa6e5a8b
BryanField15 <6367@holbertonstudents.com>
2023-04-12 15:30:20 -0700

    updated malloac for array

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_set - adds and element to the hash table
+ *@ht: hash table to be undated
+ *@key: key is the key
+ *@value: value associated with the key
+ *Return: 1 on success
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	int i;
+
+	if (key == NULL)
+	{
+		return (0);
+	}
+
+	return (1);
+}
