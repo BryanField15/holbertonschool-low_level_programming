159f1c198d46900f3da31083eda792a8c22063c1
BryanField15 <6367@holbertonstudents.com>
2023-04-11 23:40:30 -0700

    changed malloc to defref new

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
-	new = malloc(sizeof(hash_table_t));
+	new = malloc(sizeof(*new));
 	if (new == NULL)
 	{
 		return (NULL);
