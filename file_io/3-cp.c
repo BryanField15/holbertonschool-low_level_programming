5c81520eb6455c84e11cb89212a99d2a8c1c58fe
BryanField15 <6367@holbertonstudents.com>
2023-04-11 21:54:21 -0700

    fixed a bit of betty

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,21 +16,21 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit (97);
+		exit(97);
 	}
 
 	src_fd = open(argv[1], O_RDONLY);
 	if (src_fd == -1 || argv[1] == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit (98);
+		exit(98);
 	}
 
 	dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (dest_fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit (99);
+		exit(99);
 	}
 
 	read_bytes = read(src_fd, buffer, BYTES);
@@ -46,7 +46,7 @@ int main(int argc, char *argv[])
 		if (write_bytes == -1 || read_bytes != write_bytes)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit (99);
+			exit(99);
 		}
 		read_bytes = read(src_fd, buffer, BYTES);
 	}
