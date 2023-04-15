b98e2e1e8f8f430a8288b227b27ba24759464889
BryanField15 <6367@holbertonstudents.com>
2023-04-15 04:42:43 -0700

    task 4 solution

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_get - etrieves a value associated with a key.
+ *@ht: hash table to invetsigate
+ *@key: key to retrieve
+ *Return: key value or NULL on failure
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *tmp;
+
+	if (ht == NULL || key == NULL)
+	{
+		return (NULL);
+	}
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	tmp = ht->array[index];
+	if (tmp == NULL)
+	{
+		return (NULL);
+	}
+	while (strcmp(tmp->key, key) != 0)
+	{
+		tmp = tmp->next;
+	}
+	return (tmp->value);
+}
