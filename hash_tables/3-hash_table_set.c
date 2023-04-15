91b5140bc383717a0cf220ba6413b46cef2a9d96
BryanField15 <6367@holbertonstudents.com>
2023-04-15 04:04:26 -0700

    made make node function

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,4 +1,35 @@
 #include "hash_tables.h"
+/**
+ *make_node - makes a new node
+ *@key: key of node
+ *@value: value of node
+ *Return: pointer to the new node or NULL on failure
+ */
+
+hash_node_t *make_node(const char *key, const char *value)
+{
+	new_node = malloc(sizeof(*new_node));
+	if (new_node == NULL)
+	{
+		return  (NULL);
+	}
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+	return (new_node);
+}
 
 /**
  *hash_table_set - adds and element to the hash table
@@ -19,25 +50,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	index = key_index((const unsigned char *)key, ht->size);
-
-	new_node = malloc(sizeof(*new_node));
+	new_node = make_node(key, value);
 	if (new_node == NULL)
 	{
-		return  (0);
-	}
-
-	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
 		return (0);
 	}
 	new_node->next = ht->array[index];
