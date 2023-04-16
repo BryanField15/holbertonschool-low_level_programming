d32211a4edaa2a73ec0da0fef9cfe3921ef7b6b8
BryanField15 <6367@holbertonstudents.com>
2023-04-15 20:27:23 -0700

    task 6 check

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_delete - deletes a hash table
+ *@ht: has table to be deleted
+ *Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node, *temp;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+
+	i = 0;
+	while (i < ht->size)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			temp = node;
+			node = node->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+		}
+		i = i + 1;
+	}
+	free(ht->array);
+	free(ht);
+}
